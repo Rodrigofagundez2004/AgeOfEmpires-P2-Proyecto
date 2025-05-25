769d7a3743b56a125b968516713a807889bd2ec8
Rodrigofagundez2004 <yeekko19@gmail.com>
2025-05-25 17:45:59 -0300

    Estructura

---
+++ src/Library/Unidad.cs
@@ -0,0 +1,13 @@
+﻿namespace Library;
+
+public abstract class Unidad
+{
+    public string Nombre { get; set; }
+    public int Vida { get; set; }
+    public int Ataque { get; set; }
+    public int Defensa { get; set; }
+    public int Velocidad { get; set; }
+
+    public abstract Task RealizarAccion(); //Este metodo va a determinar la accion que tenga una unidad
+    public abstract Task Mover() 
+}
